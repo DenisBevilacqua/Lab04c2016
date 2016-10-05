3dab9b2d1b831c988a49198ce0cb15661d572e19
GinoRaimondi <gino4812@gmail.com>
2016-10-04 21:09:55 -0300

    Mejoras

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/MainActivity.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/MainActivity.java
@@ -180,6 +180,8 @@ public class MainActivity extends AppCompatActivity
             case R.id.nav_perfil:
                 break;
             case R.id.nav_reservas:
+                Intent i2 = new Intent(MainActivity.this,ListaReservaActivity.class);
+                startActivity(i2);
                 break;
             case R.id.nav_destinos:
                 break;
