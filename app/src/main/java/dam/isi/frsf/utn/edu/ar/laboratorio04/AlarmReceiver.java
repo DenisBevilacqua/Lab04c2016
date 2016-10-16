7152bffd2e577d0fee291aaaa1b7b9988d65ac3e
Denis Bevilacqua <denisbevilacqua6@gmail.com>
2016-10-15 21:53:27 -0300

    Notificaciones

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/AlarmReceiver.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/AlarmReceiver.java
@@ -22,22 +22,44 @@ public class AlarmReceiver extends BroadcastReceiver {
 
         // For our recurring task, we'll just display a message
 
-        long mili = System.currentTimeMillis();
+        /*long mili = System.currentTimeMillis();
+
         int milisegundos = (int) mili;
+
         Integer resto = milisegundos%2;
 
-        //int e = Log.e(milisegundos);
-
         if(resto == 0){
+
             Toast.makeText(context, "Es multiplo de 3. Se generará una notificacion." , Toast.LENGTH_SHORT).show();
-
-
         }
+
         else
+
         { Toast.makeText(context, "No se generará notificación." , Toast.LENGTH_SHORT).show();
-        }
+        }*/
+        Toast.makeText(context, "Cramos una notificacion" , Toast.LENGTH_SHORT).show();
+        showNotification(context);
 
+    }
 
+    private void showNotification(Context context) {
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, AltaReservaActivity.class), 0);
+
+        // Invoking the default notification service
+        NotificationCompat.Builder  mBuilder = new NotificationCompat.Builder(context);
+
+        mBuilder.setContentTitle("New Message with explicit intent");
+        mBuilder.setContentText("New message from javacodegeeks received");
+        mBuilder.setTicker("Explicit: New Message Received!");
+        mBuilder.setSmallIcon(R.drawable.ic_menu_camera);
+
+        mBuilder.setContentIntent(contentIntent);
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+        mBuilder.setAutoCancel(true);
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
 
     }
 }
